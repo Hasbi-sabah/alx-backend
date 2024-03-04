e4bf69a137e72397fe10664a4bde8b4510fa127e
H. Sabah <sabahhasbi00@gmail.com>
2024-03-04 20:50:45 +0000

    Code fix: minor issues

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -5,16 +5,17 @@ function createPushNotificationsJobs(jobs, queue) {
   for (let job of jobs) {
     job = queue
       .create("push_notification_code_3", job)
-      .save((err) => {
-        if (!err) console.log("Notification job created:", job.id);
-      })
+    job
       .on("complete", () => console.log(`Notification job ${job.id} completed`))
       .on("failed", (err) =>
         console.log(`Notification job ${job.id} failed: ${err}`)
       )
       .on("progress", (perc) =>
         console.log(`Notification job ${job.id} ${perc}% complete`)
-      );
+      )
+      .save(() => {
+        console.log("Notification job created:", job.id);
+      });
   }
 }
 
